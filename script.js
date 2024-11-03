9ca393b03dc4fb34dadd71f5398f2def2373a43b
Daniel <daniel.e.san.1999@gmail.com>
2024-11-03 18:02:47 -0400

    Added and initial button functionality

--- script.js
+++ script.js
@@ -1,3 +1,6 @@
+const display = document.querySelector("#inputBox");
+const btns = document.querySelectorAll("button");
+
 const add = (a, b) => {
     return a + b;
 }
@@ -28,3 +31,64 @@ const operate = (a, b, operator) => {
             return 'Syntaxis Error!';
     }
 }
+
+btns.forEach((btn) => {
+    btn.addEventListener("click", () => {
+        switch (btn.id) {
+            case 'num0':
+                display.textContent+= '0';
+                break;
+            case 'num1':
+                display.textContent+= '1';
+                break;
+            case 'num2':
+                display.textContent+= '2';
+                break;
+            case 'num3':
+                display.textContent+= '3';
+                break;
+            case 'num4':
+                display.textContent+= '4';
+                break;
+            case 'num5':
+                display.textContent+= '5';
+                break;
+            case 'num6':
+                display.textContent+= '6';
+                break;
+            case 'num7':
+                display.textContent+= '7';
+                break;
+            case 'num8':
+                display.textContent+= '8';
+                break;
+            case 'num9':
+                display.textContent+= '9';
+                break;
+            case 'dot':
+                display.textContent+= '.';
+                break;
+            case 'addOp':
+                display.textContent+= '+';
+                break;
+            case 'subsOp':
+                display.textContent+= '-';
+                break;
+            case 'multOp':
+                display.textContent+= 'x';
+                break;
+            case 'divOp':
+                display.textContent+= '/';
+                break;
+            case 'clearBtn':
+                display.textContent= '';
+                break;
+            case 'enterBtn':
+                display.textContent= operate();
+                break;
+            default:
+                display.textContent= 'ERROR!';
+                break;
+        }
+    })
+})
